4fb4f168360d4e167851bc8a8f3b218076b6322b
Oyeleke Okikioluwa Mark <oyelekeokiki@yahoo.com>
2026-06-11 18:45:26 +0100

    Initial Commit

---
+++ AssemblyAI DemoMacTests/AssemblyAI_DemoMacTests.swift
@@ -0,0 +1,17 @@
+//
+//  AssemblyAI_DemoMacTests.swift
+//  AssemblyAI DemoMacTests
+//
+//  Created by Oyeleke Okiki on 6/11/26.
+//
+
+import Testing
+@testable import AssemblyAI_DemoMac
+
+struct AssemblyAI_DemoMacTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
